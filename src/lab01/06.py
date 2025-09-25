0f0ec895ce6bbbd522c01793b96f9b2b5d27ace5
unknown <neirocult7@mail.ru>
2025-09-25 10:21:20 +0300

    ex6

--- src/lab01/06.py
+++ src/lab01/06.py
@@ -4,4 +4,4 @@ for i in range(n):
     sname, fname, age, problem = map(str, input("in_"+str(i+2)+": ").split()) 
     if problem == "True": och+=1
     else: zaoch += 1
-print(f'out {och} {zaoch}')
+print(f'out: {och} {zaoch}')
