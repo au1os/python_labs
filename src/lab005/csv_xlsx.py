38f61dc1fbec58a036d7a4c93664fb600d40678a
unknown <neirocult7@mail.ru>
2025-12-15 22:22:50 +0300

    whatswrong

---
+++ src/lab005/csv_xlsx.py
@@ -0,0 +1,28 @@
+from openpyxl import Workbook
+from pathlib import Path
+import csv
+from f_read_to_write import r_json, r_csv
+
+
+
+
+def csv_to_xlsx(csv_path: str | Path, xlsx_path: str | Path) -> None:
+    csv_data = r_csv(csv_path)
+    mas = []
+    lis = []
+    for key in csv_data[0].keys():
+        lis.append(str(key))
+    mas.append(lis)
+    lis = []
+    for row in csv_data:
+        for key in csv_data[0].keys():
+            lis.append(row[key])
+        mas.append(lis)
+        lis = []
+    with open(csv_path, encoding="utf-8") as file:
+        wb = Workbook()
+        ws = wb.active
+        ws.title = "Sheet1"
+        for row in mas:
+            ws.append(row)
+        wb.save(xlsx_path)
